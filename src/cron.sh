6f2e14221f256abc4c602111a3cb8eaaa5005088
GoAwayPeter <gaw.undefined@gmail.com>
2018-02-27 23:18:56 +0000

    more fixes

--- src/cron.sh
+++ src/cron.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-# */15 * * * * bash /django/src/cron.sh > /django/output.txt 2>&1
-# export -p >env.vars
-
-source /django/env.vars
+source /django/.env
 /usr/local/bin/python /django/src/manage.py getSensorData --settings=djangoreactredux.settings.dev_docker
