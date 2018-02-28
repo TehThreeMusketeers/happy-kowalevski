52f913c2729bf5ea2f34dee171312a04b9eedfa1
GoAwayPeter <gaw.undefined@gmail.com>
2018-02-28 21:05:59 +0000

    increased page size to 50

--- src/djangoreactredux/settings/base.py
+++ src/djangoreactredux/settings/base.py
@@ -102,7 +102,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 20,
+    'PAGE_SIZE': 50,
     'DEFAULT_PARSER_CLASSES': (
         'rest_framework.parsers.JSONParser',
     ),
